afda07c3621d0a61cb4e59847fa76996640ea8c5
Motasem-Sulaiman <motasemismael0@gmail.com>
2023-12-04 14:50:02 +0000

    1-9

---
+++ main.js
@@ -0,0 +1,21 @@
+// Single Line comment 
+// Single Line comment 
+// Single Line comment 
+/*
+1
+2
+3
+4
+ */
+
+// Ctrl + / to comment or un comment
+// Directive %c Styling console
+// window.alert("hello this is the first lecture in JS");
+document.write("<h1>hello there</h1>")
+console.log("hello.")
+console.error("err")
+console.table(["motasem","ismael","sulaiman"])
+console.log("my first %cJavaScript lecture","color:red;font-size:40px")
+/*
+ES6
+*/
