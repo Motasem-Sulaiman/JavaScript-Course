fc4649e861ca6134b0f7abe4f31d5a90c69bc92d
Motasem-Sulaiman <motasemismael0@gmail.com>
2023-12-06 14:45:45 +0000

    10-22

---
+++ datatypes.js
@@ -0,0 +1,89 @@
+document.write("<h1>second file</h1>");
+
+/*
+var ---
+Redeclare (yes)
+access before declare (Undefined)
+variable scope drama(added to window)
+Block or function Scope()
+
+let ---
+Redeclare (no-->Error)
+access before declare (Error)
+variable scope drama(No)
+Block or function Scope()
+
+
+
+const ---
+Redeclare (no-->Error)
+access before declare (Error)
+variable scope drama(No)
+Block or function Scope()
+*/
+
+/*
+String Syntax + Character Escape Sequences
+\ backslash Escape+ Line Continue
+console.log("motasem \" sulaiman")
+\n console.log("motasem\n sulaiman")
+*/
+
+
+/*
+Concatenation
+let a="hello"
+let b="there"
+document.write(a+b)
+*/
+
+/*
+` backtech
+Template Literals (Template Strings)
+let a="hello"
+let b="there"
+console.log(`${a} ${b}`)
+
+*/
+
+
+/*
+Arithmetic Operators
++ addition 
+- subtraction
+* multiplication
+/ division
+** exponentation
+% moodulus (division remainder)
+++ increment post/pre
+num++
+++num
+*/
+
+
+/*
+Unary Plus (return number-- if not number Nan)
+Unary Negation (return number and negates it)
+*/
+
+/*
+Type Coercion (Type Casting)
+let a="10"
+let b=20
+let c=true
+
+
+*/
+
+/*
+Assignment Operators
+let a=10
+a=a+20
+a+=20 // a=a+20
+a-=50
+a/=3
+a*=4
+
+*/
+
+/*
